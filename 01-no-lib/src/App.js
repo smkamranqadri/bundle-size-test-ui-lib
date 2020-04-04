6dfeb0d04f915a494cd4e7f1fa2ef75eb421c8e6
Muhammad Kamran <smkamranqadri@yahoo.com>
2020-04-04 12:17:31 +0500

    removed logo image import and changed text in 01-no-lib

--- 01-no-lib/src/App.js
+++ 01-no-lib/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <button className="btn">button</button>
+      <button className="btn">Test</button>
     </div>
     
   );
